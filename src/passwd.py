946f2fbbb05198b07c1da19a6af0b82001d9cebe
shale <slucas001@citymail.cuny.edu>
2023-08-03 11:49:55 -0400

    added 3 new functions

--- src/passwd.py
+++ src/passwd.py
@@ -8,6 +8,7 @@ import sys
 
 
 all_word_choices = []
+generated_pwd = []
 
 with open("usr/share/dict/words", 'r') as word_choices: 
     while True: 
@@ -27,4 +28,27 @@ def suffix_pwd_with_num(pwd: str) -> str:
     r_num = str(random.randint(0, 1000))
     pwd += r_num
     return pwd
+
+def clean_word(w: str) -> str: 
+    # Remove single apostrophe
+    if "'" in w:
+        new_w = w.replace("'", "")
+    
+    return new_w
+
+def select_word(l : List[str]) -> str:
+    w = random.choice(l)
+    return w
+
+def generate_pwd(length: int) -> str: 
+    for _ in range(length): 
+        word = select_word(all_word_choices)
+        generated_pwd.append(clean_word(word))
+    
+    pwd = "".join(generated_pwd)
+    return pwd
+
+
+
+
 
