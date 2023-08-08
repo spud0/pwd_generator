686b9470156f016fd79032862d1c82f11f154560
shale <slucas001@citymail.cuny.edu>
2023-08-07 21:48:33 -0400

    added grid and unorganized gui

--- src/gui.py
+++ src/gui.py
@@ -9,21 +9,33 @@ class MainWindow(Gtk.Window):
         super().__init__(title = "Pwd Gen")
         self.set_default_size(600, 400)
 
-        # The Password
         self.pwd = ""
+        self.length = 0
 
-        # Layout of the Window
+        self.title = Gtk.Label()
+        self.title.set_text("Password Generator")
         grid = Gtk.Grid()
-
-
+        
         # Number of words in Password 
         self.num_words = Gtk.Entry()
-        self.num_words.set_text("Enter the Number of Words")
+        self.num_words.set_text("")
+
+        # Field for the password
+        self.password_display = Gtk.Entry()
+        self.password_display.set_editable(False)
+
+        # If the user input isn't a number, throw exception
+        try:
+            self.length = self.num_words.get_text() 
+
+        except Exception as e: 
+            self.num_words.set_text("Error: {}".format(e))
+            self.num_words.set_editable(False)
 
         # Buttons for Password Generation and Modification
         self.generate_pwd_button = Gtk.Button(label = "Generate Password")
         self.generate_pwd_button.connect("clicked" , self.on_generate_pwd_button_clicked)
-        self.add(self.generate_pwd_button)
+        # self.add(self.generate_pwd_button)
 
         # Prefix 
         self.prefix_pwd = Gtk.CheckButton(label = "Prefix Password With Number")
@@ -33,14 +45,29 @@ class MainWindow(Gtk.Window):
         self.suffix_pwd = Gtk.CheckButton(label = "Suffix Password With Number")
         self.suffix_pwd.connect("toggled", self.on_suffix_pwd_toggled)
         
+        #Positions
+        grid.attach(self.title, 0, 1, 3, 1)
+        grid.attach(self.password_display, 1, 2, 3, 1)
+        grid.attach(self.generate_pwd_button, 2, 3, 1, 1)
+        grid.attach(self.prefix_pwd, 1, 4, 1, 1) 
+        grid.attach(self.suffix_pwd, 4, 4, 1, 1)
+        grid.attach(self.num_words, 2, 4, 1, 1) 
 
+        self.add(grid)
+    
         # Password Generator, and Modifiers
     def on_generate_pwd_button_clicked(self, widget):
-        self.pwd = generate_pwd(self.num_words)
+        self.pwd = generate_pwd(self.length)
+        self.password_display.set_text(self.pwd)
+
+        if len(self.pwd) > 0: 
+            self.num_words.set_editable(False)
 
     def on_prefix_pwd_toggled(self, button):
         if button.get_active():
-            self.pwd = prefix_pwd_with_num(self.pwd)
+           self.pwd = prefix_pwd_with_num(self.pwd)
+        
+
 
     def on_suffix_pwd_toggled(self, button):
         if button.get_active():
