06c34f503aff6784309971c8728d155c9acedbb2
shale <slucas001@citymail.cuny.edu>
2023-08-14 17:28:15 -0400

    formatted code and fixed spacing in gui

--- src/gui.py
+++ src/gui.py
@@ -6,82 +6,85 @@ from passwd import generate_pwd, prefix_pwd_with_num, suffix_pwd_with_num, remov
 class MainWindow(Gtk.Window):
     # The constructor of the parent class 
     def __init__(self): 
-        super().__init__(title = "Pwd Gen")
+        super().__init__(title = "Psuedo-Random Password Generator")
         self.set_default_size(600, 400)
 
         self.pwd = ""
         self.length = 0
 
         self.title = Gtk.Label()
-        self.title.set_text("Password Generator")
+        self.title.set_text("Password Output")
+        self.title.set_hexpand(True)
+
+        # Containers 
         grid = Gtk.Grid()
         
         # Number of words in Password 
         self.num_words = Gtk.Entry()
         self.num_words.set_text("")
         self.num_words.set_editable(True)
+        self.num_words.set_max_length(1)
 
 
-        # Field for the password
-        self.password_display = Gtk.Entry()
-        self.password_display.set_editable(False)
-
+        # Text field for the password
+        self.pwd_display = Gtk.Entry()
+        self.pwd_display.set_editable(False)
+         
         # Buttons for Password Generation and Modification
         self.generate_pwd_button = Gtk.Button(label = "Generate Password")
         self.generate_pwd_button.connect("clicked" , self.on_generate_pwd_button_clicked)
-        # self.add(self.generate_pwd_button)
+        self.generate_pwd_button.set_border_width(10)
 
         # Prefix 
         self.prefix_pwd = Gtk.CheckButton(label = "Prefix Password With Number")
         self.prefix_pwd.connect("toggled", self.on_prefix_pwd_toggled)
+        self.prefix_pwd.set_border_width(10)
 
         # Suffix
         self.suffix_pwd = Gtk.CheckButton(label = "Suffix Password With Number")
         self.suffix_pwd.connect("toggled", self.on_suffix_pwd_toggled)
-        
-        #Positions
-        grid.attach(self.title, 0, 1, 3, 1)
-        grid.attach(self.password_display, 1, 2, 3, 1)
+        self.suffix_pwd.set_border_width(10)
+ 
+        # Positions
+        grid.set_border_width(3)
+        grid.attach(self.title, 2, 1, 1, 1)
+        grid.attach(self.pwd_display, 1, 2, 4, 1)
         grid.attach(self.generate_pwd_button, 2, 3, 1, 1)
-        grid.attach(self.prefix_pwd, 1, 4, 1, 1) 
-        grid.attach(self.suffix_pwd, 4, 4, 1, 1)
-        grid.attach(self.num_words, 2, 4, 1, 1) 
-
+        grid.attach(self.prefix_pwd, 1, 7, 1, 1) 
+        grid.attach(self.suffix_pwd, 4, 7, 1, 1)
+        grid.attach(self.num_words,  2, 7, 1, 1) 
         self.add(grid)
     
-        # Password Generator, and Modifiers
-    def on_generate_pwd_button_clicked(self, widget):
         
-        self.length = self.num_words.get_text()
+    def on_generate_pwd_button_clicked(self, widget):
 
+        self.length = self.num_words.get_text()
+        
         try: 
             self.length = int(self.length)
             self.pwd = generate_pwd(self.length)
-            self.password_display.set_text(self.pwd)
+            self.pwd_display.set_text(self.pwd) 
+        except ValueError: 
+            error_msg = "Error: Non Number Input, Invalid Type Integer."
+            self.pwd_display.set_text(error_msg)
         
-        except ValueError as e: 
-            string = "Error: Non Number Input, Invalid Type Integer."
-            self.password_display.set_text(string)
-            # print(str(e))
-        
-
     def on_prefix_pwd_toggled(self, button):
         if button.get_active():
             self.pwd = prefix_pwd_with_num(self.pwd)
-            self.password_display.set_text(self.pwd)
+            self.pwd_display.set_text(self.pwd)
         
         else:
             self.pwd = remove_nums(self.pwd)
-            self.password_display.set_text(self.pwd)
+            self.pwd_display.set_text(self.pwd)
 
     def on_suffix_pwd_toggled(self, button):
         if button.get_active():
             self.pwd = suffix_pwd_with_num(self.pwd)
-            self.password_display.set_text(self.pwd)
+            self.pwd_display.set_text(self.pwd)
 
         else: 
             self.pwd = remove_nums(self.pwd)
-            self.password_display.set_text(self.pwd)
+            self.pwd_display.set_text(self.pwd)
 
 
 m = MainWindow()
